9f437a3de5658ec0ad829ea7d87b701f3801a494
buggy <rubix.bug@gmail.com>
2022-10-25 18:08:12 +0100

    added buttons

---
+++ characters.js
@@ -0,0 +1,77 @@
+function revealGoofyOne() {
+    var dots = document.getElementById("dots")
+    var moreText = document.getElementById("goofy_quest_one");
+    var btnText = document.getElementById("myBtn_one");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Reveal Steps";
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+      }
+    }
+function revealGoofyTwo() {
+    var dots = document.getElementById("dots")
+    var moreText = document.getElementById("goofy_quest_two");
+    var btnText = document.getElementById("myBtn_two");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Reveal Steps";
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+      }
+    }
+function revealGoofyThree() {
+    var dots = document.getElementById("dots")
+    var moreText = document.getElementById("goofy_quest_three");
+    var btnText = document.getElementById("myBtn_three");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Reveal Steps";
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+      }
+    }
+function revealGoofyFour() {
+    var dots = document.getElementById("dots")
+    var moreText = document.getElementById("goofy_quest_four");
+    var btnText = document.getElementById("myBtn_four");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Reveal Steps";
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+      }
+    }
+
+function revealGoofyFive() {
+    var dots = document.getElementById("dots")
+    var moreText = document.getElementById("goofy_quest_five");
+    var btnText = document.getElementById("myBtn_five");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Reveal Steps";
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+      }
+    }
+
