8ab40aa8cc7bd73120ca948ab696db6f41574ea8
buggy <rubix.bug@gmail.com>
2022-10-26 13:23:37 +0100

    added table/changed button

--- characters.js
+++ characters.js
@@ -1,77 +1,108 @@
 function revealGoofyOne() {
-    var dots = document.getElementById("dots")
+    var dots = document.getElementById("dots_one")
     var moreText = document.getElementById("goofy_quest_one");
     var btnText = document.getElementById("myBtn_one");
 
     if (dots.style.display === "none") {
         dots.style.display = "inline";
-        btnText.innerHTML = "Reveal Steps";
+        btnText.innerHTML = "+";
         moreText.style.display = "none";
       } else {
         dots.style.display = "none";
-        btnText.innerHTML = "Read less";
+        btnText.innerHTML = "-";
         moreText.style.display = "inline";
       }
     }
 function revealGoofyTwo() {
-    var dots = document.getElementById("dots")
+    var dots = document.getElementById("dots_two")
     var moreText = document.getElementById("goofy_quest_two");
     var btnText = document.getElementById("myBtn_two");
 
     if (dots.style.display === "none") {
         dots.style.display = "inline";
-        btnText.innerHTML = "Reveal Steps";
+        btnText.innerHTML = "+";
         moreText.style.display = "none";
       } else {
         dots.style.display = "none";
-        btnText.innerHTML = "Read less";
+        btnText.innerHTML = "-";
         moreText.style.display = "inline";
       }
     }
 function revealGoofyThree() {
-    var dots = document.getElementById("dots")
+    var dots = document.getElementById("dots_three")
     var moreText = document.getElementById("goofy_quest_three");
     var btnText = document.getElementById("myBtn_three");
 
     if (dots.style.display === "none") {
         dots.style.display = "inline";
-        btnText.innerHTML = "Reveal Steps";
+        btnText.innerHTML = "+";
         moreText.style.display = "none";
       } else {
         dots.style.display = "none";
-        btnText.innerHTML = "Read less";
+        btnText.innerHTML = "-";
         moreText.style.display = "inline";
       }
     }
 function revealGoofyFour() {
-    var dots = document.getElementById("dots")
+    var dots = document.getElementById("dots_four")
     var moreText = document.getElementById("goofy_quest_four");
     var btnText = document.getElementById("myBtn_four");
 
     if (dots.style.display === "none") {
         dots.style.display = "inline";
-        btnText.innerHTML = "Reveal Steps";
+        btnText.innerHTML = "+";
         moreText.style.display = "none";
       } else {
         dots.style.display = "none";
-        btnText.innerHTML = "Read less";
+        btnText.innerHTML = "-";
         moreText.style.display = "inline";
       }
     }
 
 function revealGoofyFive() {
-    var dots = document.getElementById("dots")
+    var dots = document.getElementById("dots_five")
     var moreText = document.getElementById("goofy_quest_five");
     var btnText = document.getElementById("myBtn_five");
 
     if (dots.style.display === "none") {
         dots.style.display = "inline";
-        btnText.innerHTML = "Reveal Steps";
+        btnText.innerHTML = "+";
         moreText.style.display = "none";
       } else {
         dots.style.display = "none";
-        btnText.innerHTML = "Read less";
+        btnText.innerHTML = "-";
         moreText.style.display = "inline";
       }
     }
 
+function revealGoofySix() {
+    var dots = document.getElementById("dots_six")
+    var moreText = document.getElementById("goofy_quest_six");
+    var btnText = document.getElementById("myBtn_six");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "+";
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "-";
+        moreText.style.display = "inline";
+      }
+    }
+    function revealGoofySeven() {
+      var dots = document.getElementById("dots_seven")
+      var moreText = document.getElementById("goofy_quest_seven");
+      var btnText = document.getElementById("myBtn_seven");
+  
+      if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          btnText.innerHTML = "+";
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          btnText.innerHTML = "-";
+          moreText.style.display = "inline";
+        }
+      }
+  
